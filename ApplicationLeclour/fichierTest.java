2c0eab44b8e6b9fba064f385f6c528ee8836ad95
ThierryHund <tyery1@gmail.com>
2014-05-01 21:00:31 +0200

    encore un commit toujours pour une modif du fichier test

--- ApplicationLeclour/fichierTest.java
+++ ApplicationLeclour/fichierTest.java
@@ -2,8 +2,5 @@ Essayez de modifier ce fichier - Yacine
 
 
 Je modifie ce fichier juste pour un test le mardi 29 avril à 18h30 - Max
-<<<<<<< HEAD
 
-coucou test modif boum - thierry
-=======
->>>>>>> branch 'master' of git@github.com:YacineKhamis/CCLeclour.git
+une modife qui a pas prise et que refai....thierry
