5909522dc300c7eca6c56f80e7b6f5d405473e98
ThierryHund <tyery1@gmail.com>
2014-05-18 16:59:15 +0200

    pareil pour ce fichier gestion_confirm (la j'ai remis ma version)

--- ApplicationLeclour/controleurs/administrateur/GestionUtilisateur_Confirm.php
+++ ApplicationLeclour/controleurs/administrateur/GestionUtilisateur_Confirm.php
@@ -1,38 +1,4 @@
-<<<<<<< Upstream, based on origin/master
-<?php
-require_once _PATH_ . "modele/groupe.class.php";
-require_once _PATH_ . "modele/magasin.class.php";
-require_once _PATH_ . "modele/utilisateur.class.php";
-$utilisateur = Utilisateurs::get ( $post ['login'] );
 
-
-$parameters ['user'] ['nom'] = $post ['nom'];
-$parameters ['user'] ['prenom'] = $post ['prenom'];
-$parameters ['user'] ['statut'] = $post ['statut'];
-$parameters ['user'] ['login'] = $post ['login'];
-//$parameters ['user'] ['mdp'] = $post ['mdp'];
-//$parameters ['user'] ['mdp_confirm'] = $post ['mdp_confirm'];
-$parameters ['user'] ['magasin'] = intval($post ['entite']);
-$parameters ['user'] ['groupe'] = intval($post ['profil']);
-$parameters ['user'] ['statut'] = $post ['statut'];
-
-
-
-if (($post ['mdp'] = $post ['mdp_confirm']) && $post ['mdp'] != "") {
-	$parameters ['user'] ['password'] = $post ['mdp'];
-	try {
-		Utilisateurs::modifie ( $post ['nom'], $post ['prenom'], $post ['login'],$post ['vieux_login'], $post ['mdp'], $post ['statut'], $parameters ['user'] ['groupe'], $parameters ['user'] ['magasin'] );
-	} catch ( Exception $e ) {
-		$parameters ['error'] = ($e->getMessage ());
-	}
-} else
-	try {
-		Utilisateurs::modifie ( $post ['nom'], $post ['prenom'], $post ['login'], "", $post ['statut'], $post ['profil'], $post ['entite'] );
-	} catch ( Exception $e ) {
-		$parameters ['error'] = ($e->getMessage ());
-	}
-
-=======
 <?php
 require_once _PATH_ . "modele/groupe.class.php";
 require_once _PATH_ . "modele/magasin.class.php";
@@ -69,7 +35,5 @@ if (($post ['mdp'] == $post ['mdp_confirm']) && $post ['mdp_confirm'] != "") {
 		$parameters ['error'] = ($e->getMessage ());
 		;
 	}
-
->>>>>>> cb0a5ad modif de je sais pas quoi
 $smarty->assign ( 'parameters', $parameters );
 
